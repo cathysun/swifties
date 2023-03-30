57ff08fd589a58d087897d70ab1a0277ceabe264
Cathy Sun <cathy.sun36@gmail.com>
2023-03-30 13:45:11 -0700

    New files

--- startmultiple.py
+++ startmultiple.py
@@ -4,31 +4,32 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
-# Set the URL of the page to scrape
-urls = [
-    "https://www.stubhub.com/taylor-swift-santa-clara-tickets-7-28-2023/event/151197002/?quantity=2&sections=1543880&ticketClasses=1524&seatTypes=&listingQty=",
-    "https://www.stubhub.com/taylor-swift-santa-clara-tickets-7-28-2023/event/151197002/?quantity=2&sections=1544068&ticketClasses=1524&seatTypes=&listingQty=",
-]
+# Set up the web driver
+with Service('./chromedriver') as service, webdriver.Chrome(service=service) as driver:
 
-# Set the time between scrapes (in seconds)
-scrape_interval = 300  # 5 minutes
+    # Set the URL of the page to scrape
+    urls = [
+        "https://www.stubhub.com/taylor-swift-santa-clara-tickets-7-28-2023/event/151197002/?quantity=2&sections=1543880&ticketClasses=1524&seatTypes=&listingQty=",
+        "https://www.stubhub.com/taylor-swift-santa-clara-tickets-7-28-2023/event/151197002/?quantity=2&sections=1544068&ticketClasses=1524&seatTypes=&listingQty=",
+    ]
 
-# Define a function to scrape the page and return the lowest price
-def scrape_price(url, driver):
-    driver.get(url)
-    price_elements = driver.find_elements(By.XPATH, "//div[contains(text(), '$')]")
-    prices = [float(p.text.replace(",", "").replace("$", "")) for p in price_elements]
-    lowest_price = min(prices)
-    return lowest_price
+    # Set the time between scrapes (in seconds)
+    scrape_interval = 300  # 5 minutes
 
-# Start scraping the pages and storing the data
-data = []
-while True:
-    with Service('./chromedriver') as service:
-        driver = webdriver.Chrome(service=service)
+    # Define a function to scrape the page and return the lowest price
+    def scrape_price(url):
+        driver.get(url)
+        price_elements = driver.find_elements(By.XPATH, "//div[contains(text(), '$')]")
+        prices = [float(p.text.replace(",", "").replace("$", "")) for p in price_elements]
+        lowest_price = min(prices)
+        return lowest_price
+
+    # Start scraping the pages and storing the data
+    data = []
+    while True:
         prices = []
         for url in urls:
-            price = scrape_price(url, driver)
+            price = scrape_price(url)
             prices.append(price)
             print(f"Scraped price ${price:.2f} from {url}")
         lowest_price = min(prices)
@@ -36,5 +37,4 @@ while True:
         print(f"Lowest price: ${lowest_price:.2f}")
         with open("ticket_prices_multiple.json", "w") as f:
             json.dump(data, f)
-        driver.quit()
         time.sleep(scrape_interval)

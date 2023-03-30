70deb57e580a7ffa4186d28131c9999981753778
Cathy Sun <cathy.sun36@gmail.com>
2023-03-30 14:30:52 -0700

    New files

--- startmultiple.py
+++ startmultiple.py
@@ -1,10 +1,28 @@
 import json
 import time
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-# Set up the web driver
-driver = webdriver.Chrome()
+chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+
+chrome_options = Options()
+options = [
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage"
+]
+for option in options:
+    chrome_options.add_argument(option)
+
+driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
 # Set the URL of the page to scrape
 urls = [

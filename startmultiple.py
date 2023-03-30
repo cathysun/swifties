3e1ec737d716eb65c268cd19d8886650f56573c0
Cathy Sun <cathy.sun36@gmail.com>
2023-03-30 13:42:55 -0700

    New files

--- startmultiple.py
+++ startmultiple.py
@@ -1,8 +1,8 @@
 import json
 import time
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 
 # Set the URL of the page to scrape
 urls = [
@@ -13,15 +13,8 @@ urls = [
 # Set the time between scrapes (in seconds)
 scrape_interval = 300  # 5 minutes
 
-# Specify the path to the ChromeDriver executable
-chrome_driver_path = "./chromedriver.exe"
-
-# Set up the web driver
-service = Service(chrome_driver_path)
-driver = webdriver.Chrome(service=service)
-
 # Define a function to scrape the page and return the lowest price
-def scrape_price(url):
+def scrape_price(url, driver):
     driver.get(url)
     price_elements = driver.find_elements(By.XPATH, "//div[contains(text(), '$')]")
     prices = [float(p.text.replace(",", "").replace("$", "")) for p in price_elements]
@@ -31,14 +24,17 @@ def scrape_price(url):
 # Start scraping the pages and storing the data
 data = []
 while True:
-    prices = []
-    for url in urls:
-        price = scrape_price(url)
-        prices.append(price)
-        print(f"Scraped price ${price:.2f} from {url}")
-    lowest_price = min(prices)
-    data.append({"price": lowest_price, "time": time.time()})
-    print(f"Lowest price: ${lowest_price:.2f}")
-    with open("ticket_prices_multiple.json", "w") as f:
-        json.dump(data, f)
-    time.sleep(scrape_interval)
+    with Service('./chromedriver') as service:
+        driver = webdriver.Chrome(service=service)
+        prices = []
+        for url in urls:
+            price = scrape_price(url, driver)
+            prices.append(price)
+            print(f"Scraped price ${price:.2f} from {url}")
+        lowest_price = min(prices)
+        data.append({"price": lowest_price, "time": time.time()})
+        print(f"Lowest price: ${lowest_price:.2f}")
+        with open("ticket_prices_multiple.json", "w") as f:
+            json.dump(data, f)
+        driver.quit()
+        time.sleep(scrape_interval)
